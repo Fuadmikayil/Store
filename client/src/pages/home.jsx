db4371d7857a782f87e1eaa8642e378f9a10c93a
mikayilov2006@gmail.com <mikayilov2006@gmail.com>
2024-12-15 21:08:17 +0400

    first commit

---
+++ client/src/pages/home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HomePages = () => {
+  return (
+    <h1 className='text-blue-300'>HomePages</h1>
+  )
+}
+
+export default HomePages
---
+++ client/src/routes/index.jsx
@@ -0,0 +1,8 @@
+import { createBrowserRouter } from "react-router-dom";
+import HomePage from  "../pages/home"
+export const routes = createBrowserRouter([
+    {
+        path:"/",
+        element: <HomePage/>
+    }
+])
